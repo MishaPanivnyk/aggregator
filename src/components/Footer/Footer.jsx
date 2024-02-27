92936a6ee5c532b36f590cc0f1c3cdb926f530dc
Misha Panivnyk <panivnykm@gmail.com>
2024-02-27 17:41:21 +0200

    add component/Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,186 @@
+import React, { useState, useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import {
+  FooterBox,
+  Section,
+  FooterLogoContainer,
+  FooterLogo,
+  SocList,
+  SocItem,
+  CategoryList,
+  CategoryItem,
+  CategoryItemTitle,
+  CategoryBoxItemLink,
+  CategoryItemLink,
+  Copyright,
+  CopyrightInfo,
+  CopyrightDesc,
+  CategoryContainer,
+} from './Footer.styled';
+import logo from 'img/Logo-footer.svg';
+import sprite from 'img/sprite.svg';
+import { Container } from 'components/Container/Container';
+export const Footer = ({ onClick }) => {
+  const isTabletOrDesktop = useMediaQuery({ minWidth: 768 });
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const [isOpen, setIsOpen] = useState([
+    isTabletOrDesktop,
+    isTabletOrDesktop,
+    isTabletOrDesktop,
+    isTabletOrDesktop,
+  ]);
+
+  const handleClick = index => {
+    setIsOpen(prevState => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
+  useEffect(() => {
+    setIsOpen([
+      isTabletOrDesktop,
+      isTabletOrDesktop,
+      isTabletOrDesktop,
+      isTabletOrDesktop,
+    ]);
+  }, [isTabletOrDesktop]);
+
+  return (
+    <FooterBox>
+      <Container>
+        <Section>
+          <FooterLogoContainer>
+            <FooterLogo to="/" onClick={onClick}>
+              <img src={logo} alt="logo" />
+            </FooterLogo>
+            <SocList>
+              <SocItem>
+                <svg width="18px" height="18px">
+                  <use href={sprite + '#icon-telegram'} />
+                </svg>
+              </SocItem>
+              <SocItem>
+                <svg width="18px" height="18px">
+                  <use href={sprite + '#icon-instagram'} />
+                </svg>
+              </SocItem>
+              <SocItem>
+                <svg width="18px" height="18px">
+                  <use href={sprite + '#icon-youtube'} />
+                </svg>
+              </SocItem>
+              <SocItem>
+                <svg width="18px" height="18px">
+                  <use href={sprite + '#icon-facebook'} />
+                </svg>
+              </SocItem>
+            </SocList>
+          </FooterLogoContainer>
+          <CategoryContainer>
+            <CategoryList>
+              <CategoryItem>
+                <CategoryItemTitle onClick={() => handleClick(0)}>
+                  Напрями
+                  {isMobile && (
+                    <svg width="10px" height="10px">
+                      <use
+                        href={
+                          isOpen[0]
+                            ? sprite + '#icon-footer-minus'
+                            : sprite + '#icon-footer-plus'
+                        }
+                      />
+                    </svg>
+                  )}
+                </CategoryItemTitle>
+                {isOpen[0] && (
+                  <CategoryBoxItemLink>
+                    <CategoryItemLink>IT</CategoryItemLink>
+                    <CategoryItemLink>Економіка</CategoryItemLink>
+                    <CategoryItemLink>Право</CategoryItemLink>
+                    <CategoryItemLink>Мистецтво</CategoryItemLink>
+                    <CategoryItemLink>Сфера обслуговування</CategoryItemLink>
+                  </CategoryBoxItemLink>
+                )}
+              </CategoryItem>
+              <CategoryItem>
+                <CategoryItemTitle onClick={() => handleClick(1)}>
+                  Університети
+                  {isMobile && (
+                    <svg width="10px" height="10px">
+                      <use
+                        href={
+                          isOpen[1]
+                            ? sprite + '#icon-footer-minus'
+                            : sprite + '#icon-footer-plus'
+                        }
+                      />
+                    </svg>
+                  )}
+                </CategoryItemTitle>
+                {isOpen[1] && (
+                  <CategoryBoxItemLink>
+                    <CategoryItemLink>УКД</CategoryItemLink>
+                    <CategoryItemLink>ПНУ</CategoryItemLink>
+                    <CategoryItemLink>ІФНТУНГ</CategoryItemLink>
+                  </CategoryBoxItemLink>
+                )}
+              </CategoryItem>
+              <CategoryItem>
+                <CategoryItemTitle onClick={() => handleClick(2)}>
+                  Відгуки
+                  {isMobile && (
+                    <svg width="10px" height="10px">
+                      <use
+                        href={
+                          isOpen[2]
+                            ? sprite + '#icon-footer-minus'
+                            : sprite + '#icon-footer-plus'
+                        }
+                      />
+                    </svg>
+                  )}
+                </CategoryItemTitle>
+                {isOpen[2] && (
+                  <CategoryBoxItemLink>
+                    <CategoryItemLink>УКД</CategoryItemLink>
+                    <CategoryItemLink>ПНУ</CategoryItemLink>
+                    <CategoryItemLink>ІФНТУНГ</CategoryItemLink>
+                  </CategoryBoxItemLink>
+                )}
+              </CategoryItem>
+              <CategoryItem>
+                <CategoryItemTitle onClick={() => handleClick(3)}>
+                  Рівні
+                  {isMobile && (
+                    <svg width="10px" height="10px">
+                      <use
+                        href={
+                          isOpen[3]
+                            ? sprite + '#icon-footer-minus'
+                            : sprite + '#icon-footer-plus'
+                        }
+                      />
+                    </svg>
+                  )}
+                </CategoryItemTitle>
+                {isOpen[3] && (
+                  <CategoryBoxItemLink>
+                    <CategoryItemLink>Бакалавр</CategoryItemLink>
+                    <CategoryItemLink>Магістр</CategoryItemLink>
+                  </CategoryBoxItemLink>
+                )}
+              </CategoryItem>
+            </CategoryList>
+          </CategoryContainer>
+        </Section>
+        <Copyright>
+          <CopyrightInfo>Угода користувача</CopyrightInfo>
+          <CopyrightDesc>© 2024. Logo. Всі права захищені</CopyrightDesc>
+        </Copyright>
+      </Container>
+    </FooterBox>
+  );
+};
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,139 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+export const FooterBox = styled.footer`
+  background: #000;
+`;
+export const Section = styled.section`
+  padding-top: 44px;
+`;
+export const FooterLogo = styled(NavLink)`
+  height: 19px;
+  & img {
+    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  &:hover img {
+    transform: scale(1.2);
+  }
+`;
+export const SocList = styled.ul`
+  display: flex;
+  gap: 22px;
+  justify-content: center;
+  margin-top: 27px;
+  margin-bottom: 40px;
+
+  @media only screen and (min-width: 768px) {
+  }
+`;
+export const SocItem = styled.li`
+  @media only screen and (min-width: 768px) {
+  }
+`;
+
+export const Copyright = styled.div`
+  padding-top: 19px;
+  border-top: 1px solid #2c2c2c;
+  padding-bottom: 16px;
+  display: flex;
+  flex-direction: column;
+  @media only screen and (min-width: 768px) {
+    justify-content: space-around;
+    flex-direction: initial;
+  }
+`;
+export const CopyrightInfo = styled.p`
+  color: #b5b5b5;
+  text-align: center;
+  font-family: 'Jost';
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 15px;
+  margin-bottom: 6px;
+`;
+export const CategoryList = styled.ul`
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  margin-bottom: 44px;
+  @media only screen and (min-width: 768px) {
+    align-items: initial;
+    justify-content: space-between;
+    flex-direction: initial;
+    gap: 100px;
+  }
+  @media only screen and (min-width: 1024px) {
+    gap: 220px;
+    justify-content: center;
+  }
+`;
+export const CategoryBoxItemLink = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-bottom: 30px;
+`;
+export const CategoryItem = styled.li`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 10px;
+  @media only screen and (min-width: 768px) {
+    align-items: initial;
+  }
+`;
+export const CategoryItemTitle = styled.h3`
+  color: #fff;
+  font-family: 'Open Sans';
+  text-align: left;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+  gap: 88px;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+`;
+export const CategoryItemLink = styled.a`
+  color: #a6a6a6;
+  font-family: 'Open Sans';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 15px;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+`;
+export const CopyrightDesc = styled(NavLink)`
+  color: #b5b5b5;
+  text-align: center;
+  font-family: 'Open Sans';
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 15px;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+`;
+export const FooterLogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  @media only screen and (min-width: 768px) {
+    align-items: center;
+    flex-direction: initial;
+    justify-content: space-between;
+  }
+  @media only screen and (min-width: 1024px) {
+    justify-content: space-around;
+  }
+`;
+export const CategoryContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
