dcdf1a7239b24ac85a756f6eef819d5386e9f5f7
Misha Panivnyk <panivnykm@gmail.com>
2024-03-06 18:14:08 +0200

    update/Blog, ModalBlog

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -3,6 +3,16 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { Loader } from 'components/Loader/Loader';
 import { Container } from 'components/Container/Container';
+import ErrorPage from 'pages/ErrorPage/ErrorPage';
+import {
+  BlogAuthor,
+  BlogAuthorContainer,
+  BlogContentContainer,
+  BlogImg,
+  BlogImgContainer,
+  BlogSection,
+  BlogTitle,
+} from 'pages/Blog/Blog.styled';
 const Blog = () => {
   const { id } = useParams();
   const [blog, setBlog] = useState(null);
@@ -28,17 +38,43 @@ const Blog = () => {
   if (loading) {
     return <Loader />;
   }
-  // Добавити ERROr PAGE
+
   if (!blog) {
-    return <div>Blog not found</div>;
+    return <ErrorPage />;
   }
+
+  const createMarkup = html => {
+    return { __html: html };
+  };
+
+  const formatDate = isoDate => {
+    const date = new Date(isoDate);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  };
 
   return (
     <main>
-      <Container>
-        <h1>{blog.title}</h1>
-        <p>{blog.content}</p>
-      </Container>
+      <BlogSection>
+        <Container>
+          <BlogTitle>{blog.title}</BlogTitle>
+          <BlogImgContainer>
+            <BlogImg src={blog.imageUrl} alt={blog.title} />
+            <BlogContentContainer
+              dangerouslySetInnerHTML={createMarkup(blog.content)}
+            />
+          </BlogImgContainer>
+          <BlogAuthorContainer>
+            <BlogAuthor>Автор: {blog.author}</BlogAuthor>
+            <p> {formatDate(blog.createdAt)}</p>
+          </BlogAuthorContainer>
+        </Container>
+      </BlogSection>
     </main>
   );
 };
