b8dd9ccb3b10c05931c28294d3a094accedbc7d2
Misha Panivnyk <panivnykm@gmail.com>
2024-03-20 15:36:17 +0200

    add avatar/backend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import SharedLayout from 'components/SharedLayout/SharedLayout';
 import MainPage from 'pages/MainPage/MainPage';
 import Blogs from 'pages/Blogs/Blogs';
 import Blog from 'pages/Blog/Blog';
+import Profile from 'pages/Profile/Profile';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 import { ToastContainer } from 'react-toastify';
 
@@ -16,6 +17,7 @@ export const App = () => {
           <Route exact path="/" element={<MainPage />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/blogs/:id" element={<Blog />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </SharedLayout>
--- src/components/Header/Avatar/Avatar.jsx
+++ src/components/Header/Avatar/Avatar.jsx
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Button, AvatarContainer } from './Avatar.styled';
 import { ModalAuth } from '../ModalAuth/ModalAuth';
 import { LogoutDropdown } from '../LogoutDropdown/LogoutDropdown';
+// import avatar from 'img/Avatar.jpg';
 
 export const Avatar = ({ onClick }) => {
   const [showModal, setShowModal] = useState(false);
+  // eslint-disable-next-line
+  const [profileData, setProfileData] = useState(null);
+  const [imageUrl, setImageUrl] = useState('');
+
   const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      if (token) {
+        try {
+          console.log('Token being sent:', token);
+          const response = await axios.get(
+            `${process.env.REACT_APP_BACKEND_URL}/api/profile/`,
+            {
+              headers: {
+                Authorization: `token ${token}`,
+              },
+            }
+          );
+
+          setProfileData(response.data);
+          setImageUrl(response.data.imageUrl);
+        } catch (error) {
+          console.error('There was a problem with the fetch operation:', error);
+        }
+      }
+    };
+
+    fetchProfileData();
+  }, [token]);
 
   const handleClick = () => {
     setShowModal(!showModal);
@@ -21,10 +52,8 @@ export const Avatar = ({ onClick }) => {
   return (
     <AvatarContainer>
       <Button type="button" onClick={handleClick}>
-        <img
-          src="https://www.wpthemedetector.com/ad/uploads/Gravatar-Logo.jpg"
-          alt="avatar"
-        />
+        <img src={imageUrl} alt="avatar" />
+        {/* <img src={avatar} alt="avatar" /> */}
       </Button>
       {showModal &&
         (token ? (
