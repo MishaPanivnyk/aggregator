1546290459fe46df887f7e74b66363b3883b018f
Misha Panivnyk <panivnykm@gmail.com>
2024-03-26 17:00:26 +0200

    add pages/Universities

---
+++ src/pages/Universities/Universities.styled.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+export const UniversitiesTitle = styled.h1`
+  color: #2b2b2b;
+  padding-top: 50px;
+  text-align: center;
+  font-family: 'Poppins';
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  margin-bottom: 26px;
+  @media only screen and (min-width: 768px) {
+    font-size: 30px;
+    margin-bottom: 19px;
+  }
+  @media only screen and (min-width: 1024px) {
+    margin-bottom: 29px;
+  }
+`;
+export const UniversitiesDesc = styled.p`
+  color: #2b2b2b;
+  font-family: 'Open Sans';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 27px;
+  text-align: center;
+  @media only screen and (min-width: 768px) {
+    font-size: 18px;
+  }
+  @media only screen and (min-width: 1024px) {
+  }
+`;
