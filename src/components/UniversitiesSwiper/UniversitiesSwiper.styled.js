c5a313329573d1a45518dc654fb75486833cde6f
MishaPanivnyk <panivnykm@gmail.com>
2024-03-30 01:02:02 +0200

    add hover/slide

--- src/components/UniversitiesSwiper/UniversitiesSwiper.styled.js
+++ src/components/UniversitiesSwiper/UniversitiesSwiper.styled.js
@@ -73,7 +73,11 @@ export const ImgContainer = styled.div`
   justify-content: center;
   align-items: center;
   background-color: white;
-
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover {
+    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    transform: translateY(-10px);
+  }
   @media only screen and (min-width: 768px) {
     width: 300px;
   }
