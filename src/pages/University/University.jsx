cf72b50d264741949022fe65b229ddebcae713b3
MishaPanivnyk <panivnykm@gmail.com>
2024-04-02 19:04:22 +0300

    Create University.jsx

---
+++ src/pages/University/University.jsx
@@ -0,0 +1,9 @@
+import { UniversityId } from 'components/University/University';
+const University = () => {
+  return (
+    <main>
+      <UniversityId />
+    </main>
+  );
+};
+export default University;
