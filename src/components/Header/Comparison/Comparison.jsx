cf8b9c66c0d9cfc2f0432f0b8cc5f0f36f338854
Valentyn411 <valentyn1304@gmail.com>
2024-04-06 23:28:29 +0300

    fix component/Comparison

--- src/components/Header/Comparison/Comparison.jsx
+++ src/components/Header/Comparison/Comparison.jsx
@@ -1,13 +1,41 @@
+// Comparison.jsx
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from './Comparison.styled';
 import sprite from 'img/sprite.svg';
 
 export const Comparison = ({ onClick }) => {
+  const [comparisonCount, setComparisonCount] = useState(0);
+  const token = localStorage.getItem('token');
+  useEffect(() => {
+    const fetchComparisonCount = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BACKEND_URL}/comparison/`,
+          {
+            headers: {
+              Authorization: `token ${token}`,
+            },
+          }
+        );
+        const count = response.data.length;
+        console.log(count);
+        setComparisonCount(count);
+      } catch (error) {
+        console.error('Error fetching comparison count:', error);
+      }
+    };
+
+    fetchComparisonCount();
+  }, []);
+
   return (
     <div>
       <Link to="/comparison" onClick={onClick}>
-        <svg width="28px" height="34px">
+        <svg width="25px" height="30px">
           <use href={sprite + '#icon-comparison'} />
         </svg>
+        <div className="comparison-count">{comparisonCount}</div>
       </Link>
     </div>
   );
--- src/components/Header/Comparison/Comparison.styled.js
+++ src/components/Header/Comparison/Comparison.styled.js
@@ -1,9 +1,25 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 export const Link = styled(NavLink)`
+  position: relative;
+  .comparison-count {
+    border-radius: 50%;
+    position: absolute;
+    background: #00d254;
+    width: 20px;
+    height: 20px;
+    color: #fff;
+    font-family: 'Poppins';
+    font-size: 15px;
+
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+    top: 4px;
+    right: 4px;
+    text-align: center;
+  }
   @media only screen and (min-width: 768px) {
     padding: 21px;
-    /* border-left: 1px solid #ededed;
-    border-right: 1px solid #ededed; */
   }
 `;
