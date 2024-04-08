e9611b6008500704bf3fbcd65a31218c3a8bf761
Valentyn411 <valentyn1304@gmail.com>
2024-04-08 16:27:48 +0300

    fix background

--- src/pages/ResetPassword/ResetPassword.jsx
+++ src/pages/ResetPassword/ResetPassword.jsx
@@ -1,77 +1,12 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { Container, Typography, TextField, Button } from '@mui/material';
 
-const moveBackground = keyframes`
-  from {
-    transform: translate3d(0px, 0px, 0px);
-  }
-  to {
-    transform: translate3d(1000px, 0px, 0px);
-  }
-`;
-
-const BackgroundContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-`;
-
-const BackgroundImage = styled.img`
-  height: 70vh;
-  width: 70vh;
-  position: absolute;
-  z-index: 3;
-  right: 20px;
-`;
-
-const Stars = styled.div`
-  background: black
-    url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/stars.png) repeat;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  display: block;
-  z-index: 0;
-`;
-
-const Twinkling = styled.div`
-  width: 10000px;
-  height: 100%;
-  background: transparent
-    url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/twinkling.png')
-    repeat;
-  background-size: 1000px 1000px;
-  position: absolute;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  z-index: 2;
-  animation: ${moveBackground} 70s linear infinite;
-`;
-
-const Clouds = styled.div`
-  width: 10000px;
-  height: 100%;
-  background: transparent
-    url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/clouds_repeat.png')
-    repeat;
-  background-size: 1000px 1000px;
-  position: absolute;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  z-index: 3;
-  animation: ${moveBackground} 150s linear infinite;
-`;
-
 const FormContainer = styled(Container)`
+  padding-top: 60px;
+  padding-bottom: 135px;
   position: relative;
   z-index: 2;
 `;
@@ -83,7 +18,7 @@ const FormWrapper = styled.div`
   max-width: 400px;
   padding: 16px;
   border-radius: 4px;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  box-shadow: 5px 5px 4px 6px rgba(0, 0, 0, 0.1);
   background-color: #fff;
 `;
 
@@ -123,15 +58,6 @@ const ResetPassword = () => {
 
   return (
     <main>
-      <BackgroundContainer>
-        <BackgroundImage
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png"
-          alt=""
-        />
-        <Stars />
-        <Twinkling />
-        <Clouds />
-      </BackgroundContainer>
       <FormContainer>
         <FormWrapper>
           <Typography
